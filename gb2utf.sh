c70b95d5410bdffa1e1eb86755099fca0d68cb53
Ryan Ye <yejianye@gmail.com>
2011-03-07 00:23:59 +0800

    convert text from gb2312/GBK to utf8

---
+++ gb2utf.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cp $1 $1-backup
+for encode in 'GBK' 'GB18030' 'GB2312'
+do
+	iconv -f $encode -t UTF-8 $1-backup > $1
+	if [ $? = 0 ]; then
+		rm $1-backup
+		echo "Decode: $encode. Success!"
+		exit
+	fi
+done
+
